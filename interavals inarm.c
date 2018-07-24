618913d0694317dd2fa2098b3c297dce3f65814d
15ee013 <36723461+15ee013@users.noreply.github.com>
2018-07-24 09:27:43 +0530

    Create interavals inarm.c

---
+++ interavals inarm.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+int n,m,a,temp=0,c,i;
+printf("enter the number");
+scanf("%d%d",&n,&m);
+for(i=n;i<m;i++)
+{
+    c=n;
+while(n>0)
+{
+n1=n%10;
+temp=temp+(a*a*a);
+n=n/10;
+}
+
+
+if(temp==c)
+{
+printf(" armstrong number");
+}
+else
+{
+printf(" not an armstrong");
+}
+}
+}
